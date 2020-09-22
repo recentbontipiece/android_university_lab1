b0fe0e27ba73e66c8cd981f263d89f7b81fd8544
johnstyvesvallon <styvesGIT12345!>
2020-09-21 19:21:19 -0500

    Test api url set up

--- app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
@@ -25,7 +25,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class NYTimesApiClient {
 
     // TODO: Replace the below API key with your own generated key
-    private static final String API_KEY = "<YOUR-API-KEY-GOES-HERE>";
+    private static final String API_KEY = "cihSVCnh9Y66tLUKUhXEO5nA8n5gS7Se";
     private NYTimesService nyTimesService;
 
     public NYTimesApiClient() {
